fdf5a0f80aedbf1486de23f0d7220d3a16dad62c
Igor <tovarchiigor@gmail.com>
2023-04-01 00:16:30 +0300

    destop ready

--- pet-projects/batumiCartingClub/js/slider.js
+++ pet-projects/batumiCartingClub/js/slider.js
@@ -6,6 +6,29 @@ const swiper = new Swiper(".swiper", {
 
   centeredSlides: 3,
 
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 110,
+    },
+  },
+
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
